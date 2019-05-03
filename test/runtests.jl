62e993cd447eca414c4442647083dc178adfa6de
aleant93 <aless.antelmi@gmail.com>
2019-05-03 08:37:48 +0200

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,20 @@
-using Test, SimpleTwitterAnalyzer
+using Test, DataFrames
+using SimpleTwitterAnalyzer
 
 #setup code
+path = "./data/test.json"
+tweetsdf = load_tweets(path)
 
-@testset "Test 1" begin
-	@test 4 == 2+2
+# @testset "IO" begin
+#
+# end;
+
+@testset "Analyses" begin
+	#lang distribution
+	testdf = DataFrame(:lang => ["pt", "en", "es"], :cnt => [3, 9, 1])
+	@test cntoccurences(tweetsdf, :lang) == testdf
+
+	#source distribution
+	testdf = DataFrame(:source => ["<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>"])
 	@test_throws DomainError (-1)^0.5
 end;
-
-@testset  "Test 2" begin
-	@test 1+1==2
-end;
-
